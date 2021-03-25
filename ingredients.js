950fd249365b6fe88dd4c0e2a51c43169efce15b
ISHUboy09 <78742178+ISHUboy09@users.noreply.github.com>
2021-03-25 19:19:54 +0530

    Add files via upload

---
+++ ingredients.js
@@ -0,0 +1,6 @@
+refried beans
+tomato salsa
+sour cream
+cheese
+This was one of the best tasting filling combinations and was liked by everyone at home.
+store brought taco shells or  whole wheat flatbreads
---
+++ recipe.js
@@ -0,0 +1,12 @@
+Vegetarian tacos recipe with step by step pics. Taco is a traditional Mexican recipe of corn tortillas stuffed with various types of fillings.
+
+
+Tomato salsa recipe. you can even add guacamole instead of tomato salsa or add both.
+
+Sour cream. If you do not have sour cream, use fresh curd or yogurt.
+
+Taco shells or tortillas – can be brought from stores or made at home
+
+Grated cheddar cheese or processed cheese. Cheese is optional and can be skipped.
+
+Serve vegetarian tacos as soon as you assemble them. alternatively, you can also keep the fillings in a separate bowl and let the people make the tacos for themselves.
